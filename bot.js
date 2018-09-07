f6a6eeb6ee41675ad667b9e6f1239d00d1a9bc53
DAMAR2016 <40209506+DAMAR2016@users.noreply.github.com>
2018-09-07 10:36:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -546,7 +546,6 @@ client.on('guildCreate', guild => {
 });
 
 client.on('message', message => {
-   if(!message.channel.guild) return;
 if(message.content.startsWith(prefix + 'bc')) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
 if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
