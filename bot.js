d8d5f6514e0656ae416729562fd12b08d5b4e2c8
DAMAR2016 <40209506+DAMAR2016@users.noreply.github.com>
2018-09-09 19:44:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on("message", message => {
 		   if(!message.channel.guild) return message.channel.send("This Command is Just For Servers!")
 		   var embed = new Discord.RichEmbed()
 		   .setTitle(">> ClickHere To Add" + `${client.user.username}` + " <<")
-		   .setURL("https://discordapp.com/oauth2/authorize?client_id" + `${client.user.id}` + "=486315663737094144&permissions=2080374975&scope=bot")
+		   .setURL("https://discordapp.com/oauth2/authorize?client_id=" + `${client.user.id}` + "486315663737094144&permissions=2080374975&scope=bot")
 		   .setTimestamp()
 		   .setFooter(`Requested By | ${message.author.username}`)
 		   .setColor("RANDOM")
