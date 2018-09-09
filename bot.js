650b9cc5209f14fca9785aaa89772b55418cba0b
DAMAR2016 <40209506+DAMAR2016@users.noreply.github.com>
2018-09-09 19:31:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,11 +349,11 @@ client.on("message", message => {
 		   if(!message.channel.guild) return message.channel.send("This Command is Just For Servers!")
 		   var embed = new Discord.RichEmbed()
 		   .setTitle(">> ClickHere To Add" + `${client.user.username}` + " <<")
-		   .setURL("https://discordapp.com/oauth2/authorize?client_id=" + `${client.user.id}` + "=486315663737094144&permissions=2080374975&scope=bot")
+		   .setURL("https://discordapp.com/oauth2/authorize?client_id=486315663737094144&permissions=2080374975&scope=bot" + `${client.user.id}` + "")
 		   .setTimestamp()
 		   .setFooter(`Requested By | ${message.author.username}`)
 		   .setColor("RANDOM")
-		   message.channel.send(":white_check_mark: | Check Your DM! تم الأرسال بلخاص")
+		   message.channel.send(":white_check_mark: | Check Your ! تم الأرسال بلخاص")
 		   message.author.send({embed})
 	   }
    });
